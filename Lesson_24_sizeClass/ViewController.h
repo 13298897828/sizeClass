2a55d43a88aec7c4315e43dc226be51f7cc0183f
Tianqi Zhang <1120404010@qq.com>
2015-09-18 15:27:04 +0800

    Initial Commit

---
+++ Lesson_24_sizeClass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson_24_sizeClass
+//
+//  Created by 张天琦 on 15/9/18.
+//  Copyright (c) 2015年 张天琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
